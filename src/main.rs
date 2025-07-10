5ee0b3864a85f54eb27f3f97f9166eec692432e1
七味 <heqiweii@163.com>
2025-07-10 16:04:23 +0000

    hello world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+
+use esp_idf_svc::{hal::{delay::FreeRtos, gpio::PinDriver, prelude::Peripherals}, nvs::EspDefaultNvsPartition};
 fn main() {
     // It is necessary to call this function once. Otherwise some patches to the runtime
     // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
@@ -7,4 +9,18 @@ fn main() {
     esp_idf_svc::log::EspLogger::initialize_default();
 
     log::info!("Hello, world!");
+
+    let peripherals = Peripherals::take().unwrap();
+
+    let mut led = PinDriver::output(peripherals.pins.gpio8).unwrap();
+    
+    let nvs = EspDefaultNvsPartition::take()?;
+
+    loop {
+        led.set_high().unwrap();
+        FreeRtos::delay_ms(1000);
+        led.set_low().unwrap();
+        FreeRtos::delay_ms(1000);
+        log::info!("Hello, world!");
+    }
 }
